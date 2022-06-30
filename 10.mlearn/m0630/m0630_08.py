57e4f690b4744909225695720ee1c59d7232907f
onulee <onulee@naver.com>
2022-06-30 15:59:44 +0900

    commit

---
+++ 10.mlearn/m0630/m0630_08.py
@@ -0,0 +1,21 @@
+from sklearn.neighbors import KNeighborsRegressor   # knn회귀
+from sklearn.linear_model import LinearRegression   # 선형회귀
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 50cm,100cm 농어 각각 몇g일까요?  
+# 1개의 데이터를 산점도 그래프로 출력하시오.
+
+# 1.데이터 불러오기
+perch_length = [8.4, 13.7, 15.0, 16.2, 17.4, 18.0, 18.7, 19.0, 19.6, 20.0, 21.0, 21.0, 21.0, 21.3, 22.0, 22.0, 22.0, 22.0, 22.0, 22.5, 22.5, 22.7, 23.0, 23.5, 24.0, 24.0, 24.6, 25.0, 25.6, 26.5, 27.3, 27.5, 27.5, 27.5, 28.0, 28.7, 30.0, 32.8, 34.5, 35.0, 36.5, 36.0, 37.0, 37.0, 39.0, 39.0, 39.0, 40.0, 40.0, 40.0, 40.0, 42.0, 43.0, 43.0, 43.5, 44.0]
+perch_weight = [5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 85.0, 110.0, 115.0, 125.0, 130.0, 120.0, 120.0, 130.0, 135.0, 110.0, 130.0, 150.0, 145.0, 150.0, 170.0, 225.0, 145.0, 188.0, 180.0, 197.0, 218.0, 300.0, 260.0, 265.0, 250.0, 250.0, 300.0, 320.0, 514.0, 556.0, 840.0, 685.0, 700.0, 700.0, 690.0, 900.0, 650.0, 820.0, 850.0, 900.0, 1015.0, 820.0, 1100.0, 1000.0, 1100.0, 1000.0, 1000.0]
+
+# numpy타입으로 변형
+length = np.array(perch_length)
+weight = np.array(perch_weight)
+
+# LinearRegression 회귀 70cm농어 무게를 예측하시오.
