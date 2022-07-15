88135dac1d92f0c82da7b72df3d4a1f66dc9a94b
onulee <onulee@naver.com>
2022-07-15 14:05:40 +0900

    commit

---
+++ 11.deep/d0715/d0715_04.py
@@ -0,0 +1,23 @@
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# 1. 데이터 불러오기
+
+# 2. 데이터 전처리
+# train,test데이터
+
+# train 정규화,표준화 작업
+
+# 3. label -> 원핫인코딩으로 변경
+
+# ---------------------------------
+# 4. 딥러닝 선언
+
+# 5. 딥러닝 훈련
+
+# 6. 정확도 - score 
+
+
