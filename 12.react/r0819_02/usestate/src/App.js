74616d6b1aab9bdd736e0e5577a9d1b0ab7b0650
esgo2020 <esgo2020@cafe24.com>
2022-08-24 16:58:52 +0900

    commit

--- 12.react/r0819_02/usestate/src/App.js
+++ 12.react/r0819_02/usestate/src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [text,setText] = useState('hello')
+  let txt='hello'
+  
+  let changeTxt = () =>{
+    setText('changeHello') // 글자는 changeHello변경
+    console.log(txt)   // hello출력
+  }
+  let onSubmit = ()=>{
+    alert('데이터를 전송합니다')
+  }
+
+  let onKeyUp = (event)=>{
+    console.log('input 글자입력 됨')
+    if (event.keyCode == 13){ //enter키
+      onSubmit()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onKeyUp={onKeyUp}></input>
+      <button onClick={onSubmit}>버튼클릭</button>
+      <br />
+      <br></br>
+      <span>{text}</span>
+      <button onClick={changeTxt} >글자변경</button>
     </div>
+
   );
 }
 
