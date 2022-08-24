ffef426f653dccd736664d183a5ea2b8ec9bb17e
esgo2020 <esgo2020@cafe24.com>
2022-08-24 18:00:20 +0900

    commit

---
+++ 12.react/06/useeffect/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [kossie, setKossie] = useState(0);
+  useEffect(() => {
+    console.log(count)
+  }, [count, kossie])
+
+  useEffect(() => {
+    console.log('first rendering')
+  }, []);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div className="App">
+      <h1>Kossie Coder</h1>
+      <button onClick={increment}>Click</button>
+      <button onClick={() => setKossie(kossie + 1)}>Click1</button>
+    </div>
+  );
+}
+
+export default App;
