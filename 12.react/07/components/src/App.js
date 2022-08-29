c39f2955d0496249760a55a8a177b3f0748faf46
esgo2020 <esgo2020@cafe24.com>
2022-08-29 14:55:17 +0900

    commit

---
+++ 12.react/07/components/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>main 페이지</h1>
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
